84d13493d2c29b64b9b477670209a6ce945c1a55
Jutrzenka <kacperczaja1999@gmail.com>
2022-05-13 16:15:48 +0200

    Print button is added

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,38 @@
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
+import { useReactToPrint } from 'react-to-print';
 import {LeftSection} from "./components/LeftSection/LeftSection";
 
 import "./_App.css";
 
 export const App = () => {
-    const [width, setWidth] = useState(45);
-    return (
-    <div className="App" style={{fontSize: `${width/4.5}px`}}>
-        <div className={"CV"} style={{width: `${width}%`}}>
-            <LeftSection/>
+    const [width, setWidth] = useState(30);
+    const [fontSize, setFontSize] = useState(width/4.5);
+    const componentRef = useRef<any>(null);
+
+    const print = (
+        <div style = {{  display : "none"}}>
+            <div className={"CV"} ref={componentRef}>
+                <div className={"Document"}>
+                    <LeftSection/>
+                </div>
+            </div>
         </div>
+    )
+
+    const handlePrint = useReactToPrint({
+        content: () => componentRef.current,
+        onAfterPrint: () => {setWidth(60); setFontSize(60/4.5)}
+    });
+
+    return (
+    <div className="App">
+        <button onClick={handlePrint}>PRINT</button>
+        <div className={"CV"} style={{width: `${width}%`}}>
+            <div className={"Document"} style={{fontSize: `${fontSize}px`}}>
+                <LeftSection/>
+            </div>
+        </div>
+        {print}
     </div>
   );
 }
